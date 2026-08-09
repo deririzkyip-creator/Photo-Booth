dc2bc35a79873bd901fa7ceb0463b922bd5ab7dd
deririzkyip-creator <deririzkyip@gmail.com>
2026-08-09 12:21:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/Photo-Booth/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
